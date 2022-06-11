417ebfde3749e16aa48c6be9c6813f876e334ff3
Nemanjy <nemanjamiljevic4@gmail.com>
2022-06-11 17:54:29 +0200

    Update Reader
    Updateovan reader tako da podrzava sva 4 DataSet-a

--- Reader_component/reader.py
+++ Reader_component/reader.py
@@ -9,11 +9,29 @@ HEADERSIZE = 10                                             # Velicina headera u
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)       # TCP Konekcija
 s.connect((socket.gethostname(),1235))
 
-conn = sqlite3.connect('test_database')                     # Pravljenje sqlite3 test baze
+conn = sqlite3.connect('test_database.db')                     # Pravljenje sqlite3 test baze
 c = conn.cursor()
 
 c.execute('''
-          CREATE TABLE IF NOT EXISTS kodovi
+          CREATE TABLE IF NOT EXISTS DataSet1
+          (kod TEXT, vrednost INTEGER)
+          ''')
+                    
+conn.commit()
+c.execute('''
+          CREATE TABLE IF NOT EXISTS DataSet2
+          (kod TEXT, vrednost INTEGER)
+          ''')
+                    
+conn.commit()
+c.execute('''
+          CREATE TABLE IF NOT EXISTS DataSet3
+          (kod TEXT, vrednost INTEGER)
+          ''')
+                    
+conn.commit()
+c.execute('''
+          CREATE TABLE IF NOT EXISTS DataSet4
           (kod TEXT, vrednost INTEGER)
           ''')
                     
@@ -21,7 +39,9 @@ conn.commit()
 
 
 
-while True:                                                 # Petlja za primanje i ispis poruka
+
+while True:      
+                                               # Petlja za primanje i ispis poruka
     full_msg = b''
     new_msg = True
     while True:
@@ -33,24 +53,27 @@ while True:                                                 # Petlja za primanje
         full_msg += msg
 
         if len(full_msg) - HEADERSIZE == msglen:
-
+            conn = sqlite3.connect('test_database.db')
+            c = conn.cursor()
+            
             p1 = pickle.loads(full_msg[HEADERSIZE:])
             print(p1)
-            
-            c.execute("INSERT INTO kodovi (kod, vrednost) VALUES(?,?)",(p1[0],p1[1]))
-            
-            conn.commit
 
-            c.execute('''
-            SELECT
-            kod,
-            vrednost
-            FROM kodovi
-            ''')
+            if p1[0]=="CODE_ANALOG" or p1[0]=="CODE_DIGITAL":
+                c.execute("INSERT INTO DataSet1 (kod, vrednost) VALUES(?,?)",(p1[0],p1[1]))
+                conn.commit()
+            if p1[0]=="CODE_CUSTOM" or p1[0]=="CODE_LIMITSET":
+                c.execute("INSERT INTO DataSet2 (kod, vrednost) VALUES(?,?)",(p1[0],p1[1]))
+                conn.commit()
+            if p1[0]=="CODE_SINGLENOE" or p1[0]=="CODE_MULTIPLENODE":
+                c.execute("INSERT INTO DataSet3 (kod, vrednost) VALUES(?,?)",(p1[0],p1[1]))
+                conn.commit()
+            if p1[0]=="CODE_CONSUMER" or p1[0]=="CODE_SOURCE":
+                c.execute("INSERT INTO DataSet4 (kod, vrednost) VALUES(?,?)",(p1[0],p1[1]))
+                conn.commit()
 
-            
-            print (c.fetchall())
-            
             print("Kod uspesno smesten u bazu podataka")
             new_msg = True
             full_msg = b''
+
+
